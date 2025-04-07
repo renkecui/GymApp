cbcf44dee555e72dddefb437759123a533de1ce4
Renke <renkecui@gmail.com>
2025-04-07 13:14:31 -0400

    created HomeScreen.kt

---
+++ app/src/main/java/com/example/gymapp/HomeScreen/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.example.gymapp.HomeScreen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(
+    navController: NavController,
+) {
+
+}
